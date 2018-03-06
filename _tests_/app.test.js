d708cff53a306570df2f759e5accfa4b31cf732a
Whitney Seiler <whitney.seiler@gmail.com>
2018-03-06 13:39:34 -0800

    Try shallow instead of mount

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../client/src/components/App';
 
@@ -7,12 +7,12 @@ configure({ adapter: new Adapter() });
 
 describe('Photo Gallery App', () => {
   it('should render a .slideshow class', () => {
-    const component = mount(<App />);
-    expect(component.find('.slideshow').length).toEqual(1);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.slideshow').length).toEqual(1);
   });
 
   it('should render a .gallery class', () => {
-    const component = mount(<App />);
-    expect(component.find('.gallery').length).toEqual(1);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.gallery').length).toEqual(1);
   });
 });
